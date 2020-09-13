a35257ec23c1da301397f1fc9453fb8a71786bb7
Adrian <adferove@hotmail.com>
2020-09-14 00:08:21 +1000

    JWT added

---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const User = require('../models/User');
+
+//@desc Register user
+//@route POST /api/v1/auth/register
+//@access public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  //Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+//@desc Login user
+//@route POST /api/v1/auth/login
+//@access public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Validate email and password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse(`Please provide and email and password`, 400)
+    );
+  }
+
+  //Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse(`Invalid credentials`, 401));
+  }
+
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse(`Invalid credentials`, 401));
+  }
+
+  //Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ connectDB();
 //Routes
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 const PORT = process.env.PORT || 3000;
 
@@ -38,6 +39,7 @@ app.use(express.json());
 
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 //Middleware Error
 app.use(errorHandler);
