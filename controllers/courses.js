6a9b2323fe680faa772280fc2caa857933279be0
Adrian <adferove@hotmail.com>
2020-09-13 21:28:41 +1000

    Advanced results added

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,25 +2,21 @@ const Course = require('../models/Course');
 const Bootcamp = require('../models/Bootcamp');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
+const colors = require('colors');
 
 //@desc Get courses
 //@route GET /api/v1/courses
 //@route GET /api/v1/bootcamps/:bootcampId/courses
 //@access public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+    res
+      .status(200)
+      .json({ success: true, count: courses.length, data: courses });
   } else {
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description',
-    });
+    res.status(200).json(res.advancedResults);
   }
-
-  const courses = await query;
-  res.status(200).json({ success: true, count: courses.length, data: courses });
 });
 
 //@desc Get course by id
